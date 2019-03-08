290f3cc004b4cad490c7d514d21b8d72404cabe2
owencyr <cyroc@dukes.jmu.edu>
2019-03-08 15:35:16 -0500

    integrated expiration time

--- server/src/auth/auth-service.js
+++ server/src/auth/auth-service.js
@@ -18,6 +18,7 @@ const AuthService = {
   createJwt(subject, payload) {
     return jwt.sign(payload, config.JWT_SECRET, {
       subject,
+      expiresIn: config.JWT_EXPIRY,
       algorithm: 'HS256'
     });
   },
@@ -25,7 +26,7 @@ const AuthService = {
   verifyJwt(token) {
     return jwt.verify(token, config.JWT_SECRET, {
       algorithms: ['HS256']
-    })
+    });
   },
 
   parseBasicToken(token) {
